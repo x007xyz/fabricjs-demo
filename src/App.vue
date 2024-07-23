1a967da5da9375e5454598958edd2ff6d0cd2ade
zhangxiang.chen-EFWOM <1026161550@qq.com>
2024-07-23 17:50:52 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import RemoveVideoGreen from './views/RemoveVideoGreen.vue'
+</script>
+
+<template>
+  <RemoveVideoGreen></RemoveVideoGreen>
+</template>
+
+<style scoped>
+</style>
